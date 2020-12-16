3694ebdf294f5368804bdf061cd11d84db0ef472
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-12-15 22:30:07 -0300

    Fixing html import error on task component

--- src/scripts/components/task/task.ts
+++ src/scripts/components/task/task.ts
@@ -1,12 +1,12 @@
 import { generateComponent } from '../../core/component-factory/index.js'
+import { Component } from '../../core/component-factory/types.js'
 
-function taskComponent(element: HTMLElement) {
-  const { shadowRoot } = element
+function taskComponent(element: Component) {
   const value = element.getAttribute('value')
-  const taskSpan = shadowRoot!.querySelector('span') as HTMLSpanElement
+  const taskSpan = element.select('span') as HTMLSpanElement
   
-  const btnEdit = shadowRoot!.querySelector('.btn-blue') as HTMLButtonElement
-  //const btnRemove = shadowRoot!.querySelector('.btn-red') as HTMLButtonElement
+  const btnEdit = element.select('.btn-blue') as HTMLButtonElement
+  const btnRemove = element.select('.btn-red') as HTMLButtonElement
 
   taskSpan.innerText = value as string
 
@@ -24,9 +24,11 @@ function taskComponent(element: HTMLElement) {
     btnEdit.className = 'btn-green'
     btnEdit.innerText = 'Feito'
   })
+
+  btnRemove.addEventListener('click', () => console.log('delete'))
 }
 
 generateComponent(taskComponent, {
   tag: 'app-task',
-  htmlPath: 'scripts/components/task.html'
+  htmlPath: 'scripts/components/task/task.html'
 })
