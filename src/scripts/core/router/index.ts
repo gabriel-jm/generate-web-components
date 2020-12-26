3cdda676ff911dd78c15de01b13875e1fdc808ab
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-12-26 11:53:00 -0300

    Move url handling to router and the addition of popstate event

--- src/scripts/core/router/index.ts
+++ src/scripts/core/router/index.ts
@@ -1,4 +1,19 @@
+import routes from '../../components/routes.js'
+
 export default {
+
+  addPopStateEvent(eventHandler: EventListener) {
+    window.addEventListener('popstate', eventHandler)
+  },
+  
+  handleUrl() {
+    const { pathname } = window.location
+
+    return pathname in routes
+      ? routes[pathname]
+      : routes.notFound
+  },
+
   go(path: string) {
     history.pushState(null, '', path)
     window.dispatchEvent(new Event('popstate'))
