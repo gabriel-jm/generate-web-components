e509628af6fa2a4a1abcdfdd88477c67fdb65511
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-12-27 20:45:10 -0300

    Making the dashboard to split out if no currentUser is set

--- src/scripts/components/dashboard/app-dashboard.ts
+++ src/scripts/components/dashboard/app-dashboard.ts
@@ -1,9 +1,16 @@
 import { generateComponent } from '../../core/component-factory/index.js'
 import { ComponentObject } from '../../core/component-factory/types.js'
+import router from '../../core/router/index.js'
 import { html } from '../../core/templates/index.js'
 import { globalConfigs } from '../../store/global.js'
 
 const dashboard = <ComponentObject> {
+
+  init() {
+    if(!globalConfigs.currentUser) {
+      router.go('/')
+    }
+  },
 
   render() {
     return html`
