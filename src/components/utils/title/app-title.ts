660ab315d9ef0b55f03ff1ea5d5ec1eb75a7237e
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-18 23:36:23 -0300

    Creating a user options menu

--- src/components/utils/title/app-title.ts
+++ src/components/utils/title/app-title.ts
@@ -13,12 +13,13 @@ export type TitleElement = HTMLElement & {
 type TitleComponent = ComponentObject & {
   isLoged: boolean
   readonly logInDetails: RawHTML
+  configLogInAndLogOut(): void
 }
 
 const Title = <TitleComponent> {
   isLoged: false,
 
-  init() {
+  configLogInAndLogOut() {
     globalConfigs.logOut = () => {
       this.isLoged = false
       userService.logout()
@@ -44,6 +45,21 @@ const Title = <TitleComponent> {
     globalConfigs.logIn()
   },
 
+  init() {
+    this.configLogInAndLogOut()
+
+    const userOptionsIcon = this.select('.more')
+    const userOptionsMenu = this.select('.user-options')
+
+    userOptionsIcon.addEventListener('focus', () => {
+      userOptionsMenu.classList.add('show')
+    })
+
+    userOptionsIcon.addEventListener('focusout', () => {
+      userOptionsMenu.classList.remove('show')
+    })
+  },
+
   get logInDetails() {
     return raw`
       <div class="user-details-container">
@@ -51,7 +67,18 @@ const Title = <TitleComponent> {
           ${globalConfigs.currentUser?.name as string}
         </span>
 
-        <button class="btn">Log out</button>
+        <div class="more" tabindex="0">
+          <svg width="24" height="24" viewBox="0 0 24 24" stroke="currentColor">
+            <circle cx="12" cy="calc(24 * 0.25)" r="1"></circle>
+            <circle cx="12" cy="calc(24 * 0.5)" r="1"></circle>
+            <circle cx="12" cy="calc(24 * 0.75)" r="1"></circle>
+          </svg>
+
+          <ul class="user-options">
+            <li>Settings</li>
+            <li>Log out</li>
+          </ul>
+        </div>
       </div>
     `
   },
@@ -88,11 +115,51 @@ generateComponent(Title, {
     .user-details-container {
       color: white;
       font-size: 1.1rem;
-      width: 200px;
+      width: fit-content;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0 6px;
     }
 
-    button.btn {
+    .more {
       color: white;
+      height: 24px;
+      cursor: pointer;
+      outline: 0;
+      position: relative;
+    }
+
+    .user-options {
+      --scale: 0;
+
+      position: absolute;
+      top: 8px;
+      right: 8px;
+      background-color: white;
+      border: 0;
+      border-radius: 4px;
+      box-shadow: 0 1px 2px #3335;
+      color: #333;
+      list-style: none;
+      padding: 8px 0;
+      transform: scale(var(--scale));
+      transform-origin: top right;
+      transition: transform 0.18s;
+    }
+
+    .user-options.show {
+      --scale: 1;
+    }
+
+    .user-options li {
+      font-size: 0.95rem;
+      padding: 4px 16px;
+      transition: all 0.3s;
+    }
+
+    .user-options li:hover {
+      background-color: #eee;
     }
   `
 })
