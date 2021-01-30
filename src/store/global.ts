4b20a763893adb8f5a036c55871a8ff277e19901
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-30 19:06:41 -0300

    Making the globalConfigs event driven

--- src/store/global.ts
+++ src/store/global.ts
@@ -1,10 +1,55 @@
-interface GlobalConfigs {
-  [key: string]: any
-  currentUser: { name: string, username: string } | null
-  logIn(): true | undefined
-  logOut(): void
+import { User } from './services/user-service.js'
+
+// interface GlobalConfigs {
+//   [key: string]: any
+//   currentUser: null | Omit<User, 'password'>
+//   logIn(): true | undefined
+//   logOut(): void
+//   on(eventName: string, callback: Function): void
+// }
+
+type Events = {
+  logIn: Function[]
+  logOut: Function[]
+  userChange: Function[]
 }
 
-export const globalConfigs = <GlobalConfigs> {
-  currentUser: null
+type EventsNames = keyof Events
+
+const events: Events = {
+  logIn: [],
+  logOut: [],
+  userChange: []
 }
+
+class GlobalState {
+  #user = null as null | Omit<User, 'password'>
+
+  get currentUser(): null | Omit<User, 'password'> {
+    return this.#user
+  }
+
+  set currentUser(data: null | Omit<User, 'password'>) {
+    events.userChange.forEach(fn => fn())
+    this.#user = data
+  }
+
+  on(eventName: EventsNames, callback: Function) {
+    if(!(eventName in events)) {
+      throw new Error('Invalid event name')
+    }
+
+    events[eventName].push(callback)
+  }
+
+  logIn() {
+    events.logIn.forEach(fn => fn())
+  }
+
+  logOut() {
+    events.logOut.forEach(fn => fn())
+  }
+
+}
+
+export const globalConfigs = new GlobalState()
