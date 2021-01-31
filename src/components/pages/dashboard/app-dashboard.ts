3ce135fe771839eafa8199d53d570502f6b4f0bd
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-30 23:46:30 -0300

    Refactoring dashboard

--- src/components/pages/dashboard/app-dashboard.ts
+++ src/components/pages/dashboard/app-dashboard.ts
@@ -1,25 +1,25 @@
 import { generateComponent } from '/core/component-factory/index.js'
-import { ComponentObject } from '/core/component-factory/types.js'
+import { Component } from '/core/component-factory/types.js'
 import router from '/core/router/index.js'
-import { html } from '/core/templates/index.js'
 import { globalConfigs } from '/store/global.js'
 
-type Dashboard = ComponentObject & {}
-
-const dashboard = <Dashboard> {
-  init() {
-    if(!globalConfigs.currentUser) {
-      router.go('/')
-    }
-  },
-
-  render() {
-    return html`
-      <h2>Bem vindo! ${globalConfigs.currentUser?.name as string}</h2>
-    `
+function dashboard(element: Component) {
+  if(!globalConfigs.currentUser) {
+    router.go('/')
   }
+
+  function setWellcomeMessage() {
+    const { currentUser } = globalConfigs
+    element.select('h2').innerText = `Bem vindo! ${currentUser?.name}`
+  }
+
+  globalConfigs.on('userChange', setWellcomeMessage)
+
+  setWellcomeMessage()
 }
 
 generateComponent(dashboard, {
-  tag: 'app-dashboard'
+  tag: 'app-dashboard',
+  cssPaths: ['css/styles.css'],
+  htmlPath: 'components/pages/dashboard/app-dashboard.html'
 })
