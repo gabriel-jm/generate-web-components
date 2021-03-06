72899c0013d541e7824e4f999a9d2b984a14177b
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-03-05 22:51:36 -0300

    Adding posts to the feed and its styles

--- src/components/pages/dashboard/post-feed/index.ts
+++ src/components/pages/dashboard/post-feed/index.ts
@@ -1,19 +1,43 @@
 import { generateComponent } from '/core/component-factory/index.js'
 import { Component } from '/core/component-factory/types'
-import { html, raw } from '/core/templates/index.js'
+import { css, html, raw } from '/core/templates/index.js'
 import postsService, { Post } from '/store/services/posts-service.js'
+import userService from '/store/services/user-service.js'
 
 function PostsFeed(element: Component) {
   const feedPosts = postsService.findAll() as Post[]
 
-  element.select('div').innerHTML = !feedPosts.length
+  element.select('div').innerHTML += !feedPosts.length
     ? html`<p>No posts yet!</p>`
     : html`${feedPosts.reduce((acc, value) => {
-      return raw`${acc.html}<p>${value.content} | ${value.publishingDate}</p>`
-    }, raw``)}`
+        const { content, publishingDate, userId } = value
+        const user = userService.find(userId)
+
+        return raw`
+          ${acc.html}
+          <app-post
+            content="${content}"
+            publishing-date="${publishingDate}"
+            username="${user ? user.name : ''}"
+          />
+        `
+      }, raw``)}`
 }
 
 generateComponent(PostsFeed, {
   tag: 'posts-feed',
-  htmlString: '<div></div>'
+  htmlString: '<div></div>',
+  cssString: css`
+    div {
+      display: flex;
+      margin: 12px 0;
+      gap: 12px;
+      justify-content: flex-start;
+      align-items: center;
+    }
+
+    app-post {
+      margin-bottom: 12px;
+    }
+  `
 })
